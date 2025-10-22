48fc12a40240befbc343317e7abb2b1107578de4
SZN SMNZ <rumeysa18rumeysa@gmail.com>
2025-10-22 20:59:27 +0300

    içe aktarma yolu düzeltildi

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 import chromadb
 from google import genai
-from google.genai.models import GoogleGenerativeAIEmbeddings as GGClientEmbedings
+from google.genai import GoogleGenerativeAIEmbeddings as GGClientEmbedings
 from chromadb.api.models.Collection import Collection
 
 
